1b1373e740e143d58430a7a02f049a4a68c6407c
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-10-31 14:45:25 -0700

    Hotfix: Added check for if current node is leaf

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,7 +9,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height, right_height, padding = 0;
 
 	if (tree == NULL)
 	{
@@ -19,6 +19,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
+	if (tree->left != NULL || tree->right != NULL)
+		padding = 1;
+
 	return ((left_height > right_height) ?
-		       (left_height + 1) : (right_height + 1));
+		       (left_height + padding) : (right_height + padding));
 }
