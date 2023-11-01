80e95092f9fa5c4f11fd1b8764abbca2843f775d
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2023-11-01 04:47:47 -0700

    Completed: 10-binary_tree_depth.c 11-binary_tree_size.c 12-binary_tree_leaves.c 13-binary_tree_nodes.c 14-binary_tree_balance.c 15-binary_tree_is_full.c 16-binary_tree_is_perfect.c 17-binary_tree_sibling.c 18-binary_tree_uncle.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,79 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect.
+ *
+ * @tree: Pointer to the root node of the tree to check.
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise. If tree is NULL, return 0.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height)
+	{
+		if (binary_tree_is_full(tree))
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure.
+ * Return: The height of the binary tree. If tree is NULL, return 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height)
+			? left_height + 1 : right_height + 1);
+}
+
+/**
+ * binary_tree_is_full - Checks if a binary tree is full.
+ * @tree: Pointer to the root node of the tree to check.
+ * Return: 1 if the tree is full, 0 otherwise. If tree is NULL, return 0.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left_full, right_full;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	if (tree->left && tree->right)
+	{
+		left_full = binary_tree_is_full(tree->left);
+		right_full = binary_tree_is_full(tree->right);
+		return (left_full && right_full);
+	}
+
+	return (0);
+}
